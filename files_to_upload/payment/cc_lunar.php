b3b78d856d41865ab636c471281ef345327ea776
lucionescu <lucian.ionesq@gmail.com>
2024-02-21 20:33:27 +0200

    Added customer data to request data

--- files_to_upload/payment/cc_lunar.php
+++ files_to_upload/payment/cc_lunar.php
@@ -17,9 +17,11 @@ if (!class_exists('\\Lunar\\Lunar')) {
 if ( $_SERVER['REQUEST_METHOD'] == 'POST' && defined('XCART_START')) {
 
     $api_client = new \Lunar\Lunar($module_params['param01'], null, !!$_COOKIE['lunar_testmode']);
-    
-    $currencyCode = 'USD';
+ 
+    $lunar_currency_code = $module_params['param09'];
     $order_id = $orderids[0];
+
+    $customer = fn_lunar_get_customer_data($userinfo);
 
     $products = [];
     foreach($cart['products'] as $p) {
@@ -33,26 +35,26 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' && defined('XCART_START')) {
     $lunar_args = [
         'integration' => [
             'key' => $module_params['param02'],
-            'name' => 'SHOP',
+            'name' => $config['Company']['company_name'],
             'logo' => $module_params['param03'],
         ],
         'amount' => [
-            'currency' => $currencyCode,
+            'currency' => $lunar_currency_code,
             'decimal' => (string) $cart['total_cost'],
         ],
         'custom' => [
             'orderId' => $order_id,
             'products' => $products,
             'customer' => [
-                'name' => '',
-                'email' => '',
-                'phoneNo' => '',
-                'address' => '',
+                'name' => $customer['name'],
+                'email' => $userinfo['email'],
+                'phoneNo' => $userinfo['phone'],
+                'address' => $customer['address'],
                 'ip' => func_get_valid_ip($_SERVER["REMOTE_ADDR"]),
             ],
             'platform' => [
                 'name' => 'X-Cart',
-                'version' => $version,
+                'version' => $config['version'],
             ],
             'lunarPluginVersion' => '1.0.0',
         ],
@@ -63,15 +65,15 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' && defined('XCART_START')) {
 
     if ($module_params['param04']) {
         $lunar_args['mobilePayConfiguration'] = [
-            'configurationID' => $module_params['param04'],
             'logo' => $module_params['param03'],
+            'configurationID' => $module_params['param04'],
         ];
     }
 
     $redirect_url = 'https://pay.lunar.money/?id=';
     if (!!$_COOKIE['lunar_testmode']) {
         $redirect_url = 'https://hosted-checkout-git-develop-lunar-app.vercel.app/?id='; 
-        $lunar_args['test'] = func_lunar_get_test_object($currencyCode);
+        $lunar_args['test'] = func_lunar_get_test_object($lunar_currency_code);
     }
 
     try {
@@ -114,10 +116,10 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' && defined('XCART_START')) {
 
     $api_client = new \Lunar\Lunar($module_params['param01'], null, !!$_COOKIE['lunar_testmode']);
 
-    $lunar_error = '';
-
     $lunar_txnid = x_session_get_var('_lunar_intent_id');
 
+    $lunar_error = '';
+  
     if (!empty($lunar_txnid)) {
         try {
             $trans_data = $api_client->payments()->fetch($lunar_txnid);
@@ -142,7 +144,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' && defined('XCART_START')) {
     if (!$lunar_error && !empty($trans_data['authorisationCreated'])) {
 
         if ($module_params['use_preauth'] == 'Y') {
-            $lunar_error = 'Transaction authorized.';
+            $lunar_log = 'Transaction authorized.';
 
         } else {
             try {
@@ -178,6 +180,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' && defined('XCART_START')) {
 
     $extra_order_data = [];
     $extra_order_data['lunar_txnid'] = $lunar_txnid;
+    $extra_order_data['lunar_currency'] = $module_params['param09'];
 
     if ($lunar_error){
         $bill_output['code'] = 2;
@@ -188,7 +191,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' && defined('XCART_START')) {
         func_header_location($xcart_catalogs['customer'] . '/cart.php?mode=checkout');
     } else {
         $bill_output['code'] = 1;
-        $bill_output['billmes'] = "Lunar Transaction: " . $lunar_txnid;
+        $bill_output['billmes'] = $lunar_log . " Lunar Transaction: " . $lunar_txnid;
 
         if ($order_captured){
             $extra_order_data['capture_status'] = 'C';
@@ -206,6 +209,29 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' && defined('XCART_START')) {
         $top_message = ['type' => 'E', 'content' => 'Incorrect or no order id provided'];
         func_header_location($xcart_catalogs['customer'] . '/cart.php?mode=checkout');
     }
+}
+
+ /**
+ * 
+ */
+function fn_lunar_get_customer_data($userinfo)
+{
+    $customer_data = [
+        'firstname' => !empty($userinfo['b_firstname'])   ?   $userinfo['b_firstname']  :  (!empty($userinfo['s_firstname']) ? $userinfo['s_firstname'] : ''),
+        'lastname' => !empty($userinfo['b_lastname'])   ?   $userinfo['b_lastname']  :  (!empty($userinfo['s_lastname']) ? $userinfo['s_lastname'] : ''),
+        'address' => !empty($userinfo['b_address'])   ?   $userinfo['b_address']  :  (!empty($userinfo['s_address']) ? $userinfo['s_address'] : ''),
+        'city' => !empty($userinfo['b_city'])   ?   $userinfo['b_city']  :  (!empty($userinfo['s_city']) ? $userinfo['s_city'] : ''),
+        'state' => !empty($userinfo['b_state'])   ?   $userinfo['b_state']  :  (!empty($userinfo['s_state']) ? $userinfo['s_state'] : ''),
+        'zip' => !empty($userinfo['b_zipcode'])   ?   $userinfo['b_zipcode']  :  (!empty($userinfo['s_zipcode']) ? $userinfo['s_zipcode'] : ''),
+        'country' => !empty($userinfo['b_country'])   ?   $userinfo['b_country']  :  (!empty($userinfo['s_country']) ? $userinfo['s_country'] : ''),
+        'phone' => !empty($userinfo['b_phone'])   ?   $userinfo['b_phone']  :  (!empty($userinfo['s_phone']) ? $userinfo['s_phone'] : ''),
+    ];
+
+    return [
+        'name' => $customer_data['firstname'] . ' ' . $customer_data['lastname'],
+        'address' => $customer_data['address'] . ', ' . $customer_data['city'] . ', ' . $customer_data['state'] . ', ' 
+                    . $customer_data['zip'] . ', ' . $customer_data['country'],
+    ];
 }
 
 /**
